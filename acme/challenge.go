8d20ca6e9dd4749e8e934c3b2fd123d35742493a
David Cowden <dcow@smallstep.com>
2020-05-11 21:23:55 -0700

    acme/authority: Polymorph the challenge type
    Prior to validation, we must wrap the base challenge in the correct
    concrete challenge type so that we dispatch the correct validation
    method.

--- acme/challenge.go
+++ acme/challenge.go
@@ -287,6 +287,19 @@ func unmarshalChallenge(data []byte) (challenge, error) {
 	}
 }
 
+func (bc *baseChallenge) morph() challenge {
+	switch bc.getType() {
+	case "dns-01":
+		return &dns01Challenge{bc}
+	case "http-01":
+		return &http01Challenge{bc}
+	case "tls-alpn-01":
+		return &tlsALPN01Challenge{bc}
+	default:
+		return bc
+	}
+}
+
 // Retry information for challenges is internally relevant and needs to be stored in the DB, but should not be part
 // of the public challenge API apart from the Retry-After header.
 type Retry struct {
